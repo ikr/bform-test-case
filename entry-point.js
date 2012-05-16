cf0074e166957fb950969db56c4a2ab2508e62b0
Ivan Krechetov <ivan.krechetov@gmail.com>
2012-05-16 15:29:10 +0200

    Initial

---
+++ entry-point.js
@@ -0,0 +1,25 @@
+require(
+    [
+        "jquery",
+        "backbone",
+        "backbone-forms.amd",
+        "text!form.mustache",
+        "text!fieldset.mustache",
+        "text!field.mustache"
+    ],
+
+    function ($, Backbone, Form, tmplForm, tmplFieldset, tmplField) {
+        "use strict";
+
+        // Comment this line, and the form gets rendered.
+        //
+        Form.setTemplates({form: tmplForm, fieldset: tmplFieldset, field: tmplField}, {error: "error"});
+
+        var schema = {firstName: {validators: ["required"]}, lastName: {validators: ["required"]}},
+            M = Backbone.Model.extend({schema: schema}),
+            m = new M(),
+            f = new Form({model: m});
+
+        $("body").append(f.render().el);
+    }
+);
